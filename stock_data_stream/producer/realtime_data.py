3e871b2efc93501c76820ccd3f98acedd3e248ae
Kavish Patel <kpatel6987@gmail.com>
2017-08-29 16:03:30 -0400

    realtime stream of all S&P 500 stocks every 5 min

--- stock_data_stream/producer/realtime_data.py
+++ stock_data_stream/producer/realtime_data.py
@@ -1,20 +1,24 @@
 import time
 import schedule
 from googlefinance import getQuotes
-from streamingApp.producer.kafka_producer import Producer
+from kafka_producer import Producer
 
 
 def get_realtime_data(symbol):
     return getQuotes(symbol)[0]
 
 
-def send_realtime_data(symbol, producer):
-    producer.send(get_realtime_data(symbol))
+def send_realtime_data(producer):
+    f = open("../resources/sp500list.csv")
+    symbols = f.readlines()
+    for line in symbols:
+        symbol = line.split(",")
+        producer.send(get_realtime_data(symbol[0]))
 
 
 def main(symbol):
     producer = Producer('my-topic')
-    schedule.every(1).second.do(send_realtime_data, symbol, producer)
+    schedule.every(5).minutes.do(send_realtime_data, producer)
     while True:
         schedule.run_pending()
         time.sleep(1)
